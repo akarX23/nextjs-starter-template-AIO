39b07da0714b353ae5ac464e654118cbede05bb7
Ritik Agrawal <ritikagrawal1292000@gmail.com>
2022-07-04 13:45:34 +0530

    temp changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NextNProgress from 'nextjs-progressbar'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { SessionProvider } from 'next-auth/react'
 
 import { CacheProvider, EmotionCache } from '@emotion/react'
 import { ThemeProvider } from '@mui/material/styles'
@@ -11,16 +12,23 @@ import theme from 'helpers/muiTheme'
 import 'styles/globals.css'
 import { store } from 'redux/store'
 import createEmotionCache from 'helpers/createEmotionCache'
+import { Session } from 'next-auth'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache
+  session?: Session
 }
 
 function CcWeb(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  const {
+    Component,
+    emotionCache = clientSideEmotionCache,
+    pageProps,
+    session,
+  } = props
 
   return (
     <>
@@ -33,13 +41,15 @@ function CcWeb(props: MyAppProps) {
         options={{ easing: 'ease', speed: 500 }}
       />
       <Provider store={store}>
-        <CacheProvider value={emotionCache}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </CacheProvider>
+        <SessionProvider session={session}>
+          <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={theme}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </CacheProvider>
+        </SessionProvider>
       </Provider>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import Wrapper from 'hoc/Wrapper'
 import React from 'react'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Home: React.FC = () => {
-  return <div className="padding-alignment mt-8"></div>
+  const { data: session, status } = useSession()
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 }
 
 export default Wrapper(Home)
